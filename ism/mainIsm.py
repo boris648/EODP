6008eba191c5d88f7493ba844a64b2d05c2033cb
boris648 <100471766@alumnos.uc3m.es>
2026-09-10 19:42:14 +0200

    Start Configuration

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -4,8 +4,8 @@
 from ism.src.ism import ism
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = r'C:\EODP\eodp_students\auxiliary'
-indir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-ISM\input\gradient_alt100_act150" # small scene
+auxdir = r'C:\Users\Usuario\Documents\GitHub\EODP\auxiliary'
+indir = r"C:\Users\Usuario\Documents\GitHub\EODP\EODP_TER_2021" # small scene
 outdir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-ISM\myoutput"
 
 # Initialise the ISM
