e84801b6ebdee11abdce99cfca5f4d9e2c0f35c5
boris648 <100471766@alumnos.uc3m.es>
2026-09-24 20:00:26 +0200

    defocus MTF

--- ism/src/mtf.py
+++ ism/src/mtf.py
@@ -132,7 +132,8 @@ class mtf:
         :return: Defocus MTF
         """
         #TODO
-
+        x = np.pi * defocus * fr2D * (1 - fr2D)
+        Hdefoc = 2 * j1(x) / x
         return Hdefoc
 
     def mtfWfeAberrations(self, fr2D, lambd, kLF, wLF, kHF, wHF):
