450294c667f7e94a8058415e1e05ec599ced856c
boris648 <100471766@alumnos.uc3m.es>
2026-09-24 19:02:01 +0200

    freq2D done

--- ism/src/mtf.py
+++ ism/src/mtf.py
@@ -92,6 +92,23 @@ class mtf:
         :return fnAlt: 1D normalised frequencies 2D ALT (f/(1/w))
         """
         #TODO
+        fstepAlt = 1 / nlines / w
+        fstepAct = 1 / ncolumns / w
+
+        eps = 1e-6
+        fAlt = np.arange(-1 / (2 * w), 1 / (2 * w) - eps, fstepAlt)
+        fAct = np.arange(-1 / (2 * w), 1 / (2 * w) - eps, fstepAct)
+
+        [fAltxx, fActxx] = np.meshgrid(fAlt, fAct, indexing='ij')
+        f2D = np.sqrt(fAltxx * fAltxx + fActxx * fActxx)
+
+        fc = D/(lambd*focal)
+
+        fn2D = f2D/(1/w)
+        fr2D = f2D/fc
+        fnAct = fAlt/(1/w)
+        fnAlt = fAct/(1/w)
+
         return fn2D, fr2D, fnAct, fnAlt
 
     def mtfDiffract(self,fr2D):
