16f75ce98ec7b8c4942a891320746822326020cd
boris648 <100471766@alumnos.uc3m.es>
2026-09-24 20:09:44 +0200

    detector MTF

--- ism/src/mtf.py
+++ ism/src/mtf.py
@@ -159,6 +159,7 @@ class mtf:
         :return: detector MTF
         """
         #TODO
+        Hdet = abs(np.sinc(fn2D))
         return Hdet
 
     def mtfSmearing(self, fnAlt, ncolumns, ksmear):
