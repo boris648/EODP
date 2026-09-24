069fcc96d978b2f2f4af6981c5d16999a0cb3866
boris648 <100471766@alumnos.uc3m.es>
2026-09-24 20:21:20 +0200

    rad2Irrad fixed

--- ism/src/opticalPhase.py
+++ ism/src/opticalPhase.py
@@ -93,7 +93,7 @@ class opticalPhase(initIsm):
         :return: TOA image in irradiances [mW/m2]
         """
         # TODO
-        #toa = toa*Tr*0.25*np.pi*(D/f)^2
+        toa = toa*Tr*0.25*np.pi*(D/f)**2
         return toa
 
 
